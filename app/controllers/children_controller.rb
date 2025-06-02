e91496a66b687c207d43d527a2bda3d227932827
Alexandre Blaiset <alexandre.blaiset@gmail.com>
2025-06-02 15:16:14 +0200

    children and feeds controllers

--- app/controllers/children_controller.rb
+++ app/controllers/children_controller.rb
@@ -1,13 +1,42 @@
 class ChildrenController < ApplicationController
   def index
+    set_mother
+    @children = @mother.children
   end
 
   def new
+    @mother = User.find(params[:user_id])
+    @child = Child.new
   end
 
   def create
+    set_mother
+    @child = Child.new(child_params)
+    @child.mother = @mother
+    if @child.save
+      redirect_to user_children_path(@mother), notice: 'Child was successfully created.'
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def destroy
+    set_child
+    @child.destroy
+    redirect_to user_children_path(current_user), notice: 'Child was successfully deleted.'
+  end
+
+  private
+
+  def child_params
+    params.require(:child).permit(:name)
+  end
+
+  def set_mother
+    @mother = current_user
+  end
+
+  def set_child
+    @child = Child.find(params[:id])
   end
 end
--- app/controllers/feeds_controller.rb
+++ app/controllers/feeds_controller.rb
@@ -1,22 +1,57 @@
 class FeedsController < ApplicationController
   def index
+    @feeds = Feed.all
   end
 
   def show
+    @feed = Feed.find(params[:id])
+    @child = @feed.child
   end
 
   def new
+    @feed = Feed.new
+    @children = current_user.children
   end
 
   def create
+    @feed = Feed.new(feed_params)
+    @feed.mother = current_user
+    if @feed.save
+      redirect_to feed_path(@feed), notice: 'Feed was successfully created.'
+    else
+      @children = current_user.children
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def edit
+    set_feed
+    @children = current_user.children
   end
 
   def update
+    set_feed
+    if @feed.update(feed_params)
+      redirect_to feed_path(@feed), notice: 'Feed was successfully updated.'
+    else
+      @children = current_user.children
+      render :edit, status: :unprocessable_entity
+    end
   end
 
   def destroy
+    set_feed
+    @feed.destroy
+    redirect_to feeds_path, notice: 'Feed was successfully deleted.'
+  end
+
+  private
+
+  def feed_params
+    params.require(:feed).permit(:quantity_left, :quantity_right, :time_left, :time_right, :mood, :type)
+  end
+
+  def set_feed
+    @feed = Feed.find(params[:id])
   end
 end
