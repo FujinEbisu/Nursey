c1962ec81604007a4a7189ac270d23204957b311
FujinEbisu <leo.gover52@gmail.com>
2025-06-03 12:16:18 +0200

    setup Homepage as LP

--- app/javascript/controllers/menu_bar_controller.js
+++ app/javascript/controllers/menu_bar_controller.js
@@ -2,12 +2,13 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="menu-bar"
 export default class extends Controller {
-  static targets = ["menu"]
+  static targets = ["menu", "dropdown"]
   connect() {
     console.log("Menu bar controller connected");
   }
 
   toggle() {
-    this.menuTarget.classList.toggle('active');
+    console.log("toggle");
+    this.dropdownTarget.classList.toggle('active');
   }
-}
+} 
