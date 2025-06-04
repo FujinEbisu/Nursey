9e8e42e82ca7264177cb9a6ad867a689f7310648
Alexandre Blaiset <alexandre.blaiset@gmail.com>
2025-06-04 18:07:28 +0200

    try to fix new feed

--- app/javascript/controllers/chronometer_controller.js
+++ app/javascript/controllers/chronometer_controller.js
@@ -2,12 +2,13 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="chronometer"
 export default class extends Controller {
-    static targets = ["output", "tirage", "droite", "gauche", "stop", "chooseSide", "title", "second", "output2","newtitle"]
+    static targets = ["output", "tirage", "droite", "gauche", "stop", "chooseSide", "title", "second", "output2","newtitle", "time", "change"]
 
   connect() {
     this.startTime = Date.now()
     this.timer = setInterval(() => this.update(), 1000)
   }
+
 
   disconnect() {
     clearInterval(this.timer)
@@ -28,37 +29,47 @@ export default class extends Controller {
     const seconds1 = String(elapsed1 % 60).padStart(2, '0')
     this.output2Target.textContent = `${this.pad(minutes1)}:${this.pad(seconds1)}`
   }
-
+ stop1() {
+    clearInterval(this.timer)
+  }
 
   display(event) {
     const button = event.target.innerText
     if (button === "Tirage") {
       this.tirageTarget.classList.add("d-none")
+      this.chooseSideTarget.classList.add("d-none")
       this.titleTarget.innerText = "Nouveau tirage"
     } else if (button === "Droite") {
       this.droiteTarget.classList.add("d-none")
       this.gaucheTarget.classList.add("d-none")
-      this.chooseSideTarget.classList.remove("d-none")
-      this.secondTarget.classList.remove("d-none")
-      this.newtitleTarget.innerText = "Temps seins droit"
-      const timegauche = this.outputTarget.textContent
-         this.startTime2 = Date.now()
-        this.timer2 = setInterval(() => this.update(), 1000)
-      const timedroite = this.output2Target.textContent
+      this.timeTarget.innerText = "1er sein"
+      this.changeTarget.classList.remove("d-none")
     } else if (button === "Gauche") {
       this.gaucheTarget.classList.add("d-none")
       this.droiteTarget.classList.add("d-none")
       this.stopTarget.classList.remove("d-none")
-      this.secondTarget.classList.remove("d-none")
-      this.newtitleTarget.innerText = "Temps seins gauche"
-      this.startTime2 = Date.now()
-      this.timer2 = setInterval(() => this.update(), 1000)
-      const timedroite = this.outputTarget.textContent
-      const timegauche = this.output2Target.textContent
+      this.timeTarget.innerText = "1er sein"
     } else if (button === "Stop") {
       const time = this.outputTarget.textContent
       const timenext = this.output2Target.textContent
+    } else if (button === "Changer de seins") {
+      this.secondTarget.classList.add("d-none")
+      this.changeTarget.classList.add("d-none")
+      this.chooseSideTarget.classList.add("d-none")
+      this.secondTarget.classList.remove("d-none")
+      this.newtitleTarget.innerText = "2ème sein"
+      clearInterval(this.timer)
+       this.startTime2 = Date.now()
+      this.timer2 = setInterval(() => this.update(), 1000)
+      const timedroite = this.outputTarget.textContent
+      const timegauche = this.output2Target.textContent
     }
   }
+
+
+  stop2() {
+    clearInterval(this.timer2)
+  }
+
 }
 // this.outputTarget.classList.remove("hidden")
