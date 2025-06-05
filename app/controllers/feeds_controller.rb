1595dacec286098ebf3183cb166c977e5a57303a
Alexandre Blaiset <alexandre.blaiset@gmail.com>
2025-06-05 12:34:04 +0200

    index feeds in progress, add scenario in feed new

--- app/controllers/feeds_controller.rb
+++ app/controllers/feeds_controller.rb
@@ -38,11 +38,13 @@ class FeedsController < ApplicationController
   end
 
   def edit
+    @mother = current_user.userable
     set_feed
-    @children = current_user.children
+    @children = @mother.children
   end
 
   def update
+    @mother = current_user.userable
     set_feed
     if @feed.update(feed_params)
       redirect_to feed_path(@feed), notice: 'Feed was successfully updated.'
@@ -53,6 +55,7 @@ class FeedsController < ApplicationController
   end
 
   def destroy
+    @mother = current_user.userable
     set_feed
     @feed.destroy
     redirect_to feeds_path, notice: 'Feed was successfully deleted.'
