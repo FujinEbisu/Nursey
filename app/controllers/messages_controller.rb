8cbd7c2977c8234a1527ed3585db3435f348c1d9
FujinEbisu <leo.gover52@gmail.com>
2025-06-05 18:03:56 +0200

    Chat Style

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,6 +1,11 @@
 class MessagesController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_mother
+  before_action :set_message, only: [:show, :update]
+  before_action :set_doctor, only: [:show, :update]
+
+
   def index
-    @mother = current_user
     @messages = @mother.messages
   end
 
@@ -13,7 +18,7 @@ class MessagesController < ApplicationController
 
   def create
     @message = Message.new(message_params)
-    @message.mother = current_user
+    @message.mother = @mother
 
     if @message.save
       redirect_to messages_path, notice: 'Message was successfully created.'
@@ -34,11 +39,19 @@ class MessagesController < ApplicationController
 
   private
 
+  def set_mother
+    @mother = current_user.userable if current_user.userable.is_a?(Mother)
+  end
+
   def message_params
     params.require(:message).permit(:content)
   end
 
   def set_message
     @message = Message.find(params[:id])
+  end
+
+  def set_doctor
+    @doctor = current_user.userable if current_user.userable.is_a?(Doctor)
   end
 end
