650bc1e957d8ddef619ddec872ee44714f6cd086
Marianne Demaiziere <mariannedemaiziere@gmail.com>
2025-06-09 17:35:47 +0200

    added a color changing theme depending on wether user is a doctor or a mother

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  def theme_class
+    if current_user.userable.is_a?(Doctor)
+      return 'doctor-theme'
+    elsif current_user.userable.is_a?(Mother)
+      return 'mother-theme'
+    else
+      return ''
+    end
+  end
 end
