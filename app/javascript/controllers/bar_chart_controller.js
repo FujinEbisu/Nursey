ea1a706e560dbb49abd816870ddfb57447a5c553
Alexandre Blaiset <alexandre.blaiset@gmail.com>
2025-06-09 16:45:23 +0200

    resolve issue with avail on doctor

---
+++ app/javascript/controllers/bar_chart_controller.js
@@ -0,0 +1,8 @@
+import { Controller } from "@hotwired/stimulus"
+import { Chart } from "chart.js";
+import * as Chartjs from "chart.js";
+
+export default class extends Controller {
+  connect() {
+  }
+}
