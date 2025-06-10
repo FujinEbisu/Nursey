034f5ca72e039180d6328f907c85353a4064db3c
FujinEbisu <leo.gover52@gmail.com>
2025-06-10 13:45:24 +0200

    fixed scroll to bottom when new message, fixed send message on 'enter'

--- app/javascript/controllers/reset_form_controller.js
+++ app/javascript/controllers/reset_form_controller.js
@@ -1,7 +1,17 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
+  static targets = ["container"]
   reset() {
     this.element.reset()
+  }
+
+  handleEnter(event) {
+    if (event.key === 'Enter') {
+      this.element.submit()
+      this.containerTarget.scrollTo({
+        top: this.containerTarget.scrollHeight,
+      })
+    }
   }
 }
